95f7e7e210e91b3becdd470f00111f16cba8c47d
arif-ahmed <khandaker.arif.ahmed@gmail.com>
2025-04-02 21:13:12 +0600

    updated

---
+++ src/Catalog.API/Controllers/BooksController.cs
@@ -0,0 +1,41 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly CatalogDbContext _context;
+
+        public BooksController(CatalogDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Get()
+        {
+            var books = _context.Books.ToList();
+            return Ok(books);
+        }
+        public IActionResult Get(Guid id)
+        {
+            var book = _context.Books.Find(id);
+            return Ok(book);
+        }
+        public IActionResult Post([FromBody] Book book)
+        {
+            return Ok();
+        }
+        public IActionResult Put(Guid id, [FromBody] Book book)
+        {
+            return Ok();
+        }
+        public IActionResult Delete(Guid id) 
+        {
+            return Ok();
+        }
+    }
+}
